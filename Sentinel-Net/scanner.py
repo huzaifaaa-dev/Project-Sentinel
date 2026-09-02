66d37e93289df86e2698e6978a1c02b4e31b72b4
huzaifaaa-dev <306003782+huzaifaaa-dev@users.noreply.github.com>
2026-09-03 00:42:08 +0500

    v0.1: TCP port scanner over 1-1024 using socket + loop

---
+++ Sentinel-Net/scanner.py
@@ -0,0 +1,16 @@
+import socket
+
+target = "127.0.0.1"
+
+found = False
+
+for port in range(1, 1025):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(0.5)
+        result = sock.connect_ex((target, port))
+        if result == 0:
+                print(f"Port {port}: OPEN")
+                found = True
+
+if not found:
+        print("No open ports found.")
